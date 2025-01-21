3f3627ee96399e4f021cdaab6c95d9ab5599542f
NIILLKA <149934761+NIILLKA@users.noreply.github.com>
2025-01-21 17:36:09 +0500

    Update!!
    Bugs with the carousel were fixed and it became better. All that's left is to set up the database and a couple more things... good way to go

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel();
 });
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
