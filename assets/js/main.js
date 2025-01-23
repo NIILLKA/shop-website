8c838beaebfa3024ce7fac3073e33f4b92a3c0a3
NIILLKA <149934761+NIILLKA@users.noreply.github.com>
2025-01-23 09:36:31 +0500

    Update)

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,19 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownToggle = document.querySelector(".dropdown-toggle");
+  const dropdownMenu = document.querySelector(".dropdown-menu");
+
+  dropdownToggle.addEventListener("click", function () {
+    dropdownMenu.classList.toggle("show");
+  });
+
+  // Закрытие меню при клике вне его
+  document.addEventListener("click", function (event) {
+    if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+      dropdownMenu.classList.remove("show");
+    }
+  });
+});
